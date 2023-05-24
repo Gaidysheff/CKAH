2f4b4bbe8ce68603ad20eb070b1d723fcb17d6e9
Gaidysheff <gaidysheff@mail.ru>
2023-05-24 21:44:12 +0500

    Carousel is improved

--- frontend/src/pages/home/components/carousel/Carousel.tsx
+++ frontend/src/pages/home/components/carousel/Carousel.tsx
@@ -1,29 +1,37 @@
 import "./Carousel.scss";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 import CarouselSvgSelector from "./CarouselSvgSelector";
 import Slider from "react-slick";
-import { useState } from "react";
 
-// type Props = { slides: any };
+type Props = {};
 
-const Carousel = (slides: any) => {
-  const sliderSettings = {
-    infinite: false,
+const Carousel = (props: Props) => {
+  var settings = {
+    dots: false,
+    infinite: true,
     slidesToShow: 3,
     slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1050,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2,
+        },
+      },
+      {
+        breakpoint: 730,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
   };
 
-  const [currentIndex, setCurrentIndex] = useState(0);
-  const goToPrevious = () => {
-    const isFirstSlide = currentIndex === 0;
-    const newIndex = isFirstSlide ? slides.length - 1 : currentIndex - 1;
-    setCurrentIndex(newIndex);
-  };
-  const goToNext = () => {
-    const isLastSlide = currentIndex === slides.length - 1;
-    const newIndex = isLastSlide ? 0 : currentIndex + 1;
-    setCurrentIndex(newIndex);
-  };
   const cardContent = [
     {
       id: 1,
@@ -42,27 +50,36 @@ const Carousel = (slides: any) => {
       description:
         "Защита конфеденциальных сведений, не подлежащих разглашению по федеральному законодательству",
     },
+    {
+      id: 4,
+      icon: <CarouselSvgSelector id="speed" />,
+      description: "Высокая и оперативная скорость обработки заявки",
+    },
+    {
+      id: 5,
+      icon: <CarouselSvgSelector id="database" />,
+      description:
+        "Огромная комплексная база данных, обеспечивающая объективный ответ на запрос",
+    },
+    {
+      id: 6,
+      icon: <CarouselSvgSelector id="security" />,
+      description:
+        "Защита конфеденциальных сведений, не подлежащих разглашению по федеральному законодательству",
+    },
   ];
   return (
-    <div className="carousel">
-      <div className="carousel__leftArrowStyles" onClick={goToPrevious}>
-        ❰
-      </div>
-      <div className="carousel__rightArrowStyles" onClick={goToNext}>
-        ❱
-      </div>
-      <div className="carousel__slider">
-        <Slider {...sliderSettings}>
-          {cardContent.map((card, index) => (
-            <div className="carousel__slide">
-              <div key={index}>
-                <div className="carousel__icon">{card.icon}</div>
-                <p className="carousel__description">{card.description}</p>
-              </div>
+    <div className="container">
+      <Slider {...settings}>
+        {cardContent.map((card, index) => (
+          <div className="carousel__slide">
+            <div key={index} className="carousel__inner">
+              <div className="carousel__icon">{card.icon}</div>
+              <p className="carousel__description">{card.description}</p>
             </div>
-          ))}
-        </Slider>
-      </div>
+          </div>
+        ))}
+      </Slider>
     </div>
   );
 };
--- frontend/src/pages/home/components/section_II/Section_II.tsx
+++ frontend/src/pages/home/components/section_II/Section_II.tsx
@@ -1,10 +1,6 @@
 import Carousel from "../carousel/Carousel";
-import CarouselTest from "../carousel/CarouselTest";
 import Section_II_SvgSelector from "./Section_II_SvgSelector";
 import style from "./Section_II.module.scss";
-
-// import WhyWeCarousel from "../slider/WhyWeCarousel";
-// import slides from "../slider/slides";
 
 type Props = {};
 
@@ -14,9 +10,7 @@ const Section_II = (props: Props) => {
       <div className={style.sectionII}>
         <h2 className={style.sectionII__heading}>Почему именно мы</h2>
         <div className={style.sectionII__carousel}>
-          {/* <Carousel /> */}
-          <CarouselTest />
-          {/* <WhyWeCarousel slides={slides} /> */}
+          <Carousel />
         </div>
         <div className={style.sectionII__image}>
           <Section_II_SvgSelector id="outcome" />
